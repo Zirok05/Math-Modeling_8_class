a5890f9b5cb115f3fc4da349b76f9f7d3dee6b58
Zirok05 <56187163+Zirok05@users.noreply.github.com>
2020-02-12 18:18:16 +0200

    Add files via upload

--- temp.py
+++ temp.py
@@ -5,70 +5,90 @@ from matplotlib.animation import ArtistAnimation
 
 seconds_in_year = 365*24*60*60
 seconds_in_day  = 24*60*60
-years = 2
+years = 5
 
 t = np.arange(0,years*seconds_in_year,seconds_in_day)
 
-def PPC(z,t):
-    
-    (x_neptun,v_x_neptun, y_neptun,v_y_neptun,
-    x_plyton,v_x_plyton,y_plyton,v_y_plyton) = z
+def GG(s,t):
+    (x1,v_x1,y1,v_y1,
+     x2,v_x2,y2,v_y2,
+     x3,v_x3,y3,v_y3) = s
      
-    dxdt_neptun = v_x_neptun
-    dv_xdt_neptun = -G * sun_mass * x_neptun / (x_neptun**2 + y_neptun**2)**1.5
-    dydt_neptun = v_y_neptun
-    dv_ydt_neptun = -G * sun_mass * y_neptun / (x_neptun**2 + y_neptun**2)**1.5
-    
-    dxdt_plyton = v_x_plyton
-    dv_xdt_plyton = -G * sun_mass * x_plyton /(x_plyton**2 + y_plyton**2)**1.5
-    dydt_plyton = v_y_plyton
-    dv_ydt_plyton = -G + sun_mass *y_plyton /(x_plyton**2 + y_plyton**2)**1.5
-    
-    return (dxdt_neptun,dv_xdt_neptun,dydt_neptun,dv_ydt_neptun,
-             dxdt_plyton, dv_xdt_plyton,dydt_plyton,dv_ydt_plyton)
-    
-x0_neptun = 4503443661000
-v_x0_neptun =  0
-y0_neptun = 0
-v_y0_neptun = 5434.9
+    dxdt1 = v_x1
+    dv_xdt1 = (-G * m2 * (x1 - x2) / ((x1-x2)**2 + (y1-y2)**2)**1.5
+                -G * m3 * (x1 - x3) / ((x1-x3)**2 + (y1-y3)**2)**1.5)
 
-x0_plyton = 5868000000000
-v_x0_plyton = 0
-y0_plyton = 0
-v_y0_plyton =  4669.1
+    dydt1 = v_y1
+    dv_ydt1 = (-G * m2 * (y1 - y2) / ((x1-x2)**2 + (y1-y2)**2)**1.5
+               -G * m2 * (y1 - y3) / ((x1-x3)**2 + (y1-y3)**2)**1.5)
+ 
+    dxdt2 = v_x2
+    dv_xdt2 = (-G * m1 * (x2 - x1) / ((x2-x1)**2 + (y2-y1)**2)**1.5
+                -G * m3 * (x2 - x3) / ((x2-x3)**2 + (y2-y3)**2)**1.5)
+    dydt2 = v_y2
+    dv_ydt2 = (-G * m1 * (y2 - y1) / ((x2-x1)**2 + (y2-y1)**2)**1.5
+                -G * m2 * (y2 - y3) / ((x2-x3)**2 + (y2-y3)**2)**1.5)
+     
+    dxdt3 = v_x3
+    dv_xdt3 = (-G * m1 * (x3 - x1) / ((x3-x1)**2 + (y3-y1)**2)**1.5
+                 -G * m2 * (x3 - x2) / ((x3-x2)**2 + (y3-y2)**2)**1.5)
+    dydt3 = v_y3
+    dv_ydt3 = (-G * m1 * (y3 - y1) / ((x3-x1)**2 + (y3-y1)**2)**1.5
+                -G * m2 * (y3 - y2) / ((x3-x2)**2 + (y3-y2)**2)**1.5)
+                
+    return (dxdt1,dv_xdt1,dydt1,dv_ydt1,
+            dxdt2,dv_xdt2,dydt2,dv_ydt2,
+            dxdt3,dv_xdt3,dydt3,dv_ydt3,)
 
-z0 = (x0_neptun,v_x0_neptun,y0_neptun,v_y0_neptun,
-      x0_plyton,v_x0_plyton,y0_plyton,v_y0_plyton)
-
+aye = 149.6*10**9
 G = 6.67 * 10**(-11)
-sun_mass = 1.9 * 10**30
+M = 1.98847*10**30
+m1= 1.06*M
+m2=0.6*M
+m3=0.3*M
 
-sol = odeint(PPC,z0,t)
+
+x10 = 0
+v_x10 = -10000
+y10 = 4*aye
+v_y10 = 0
+
+x20 = 0
+v_x20 = -15000 
+y20 = 0
+v_y20 = 0
+                       
+x30 = 0
+v_x30 = -20000
+y30 = -8.3*aye
+v_y30 = 0
+s0 = (x10,v_x10,y10,v_y10,
+      x20,v_x20,y20,v_y20,
+      x30,v_x30,y30,v_y30,)
+
+
+sol = odeint(GG,s0,t)
+
 
 fig = plt.figure()
-planets = []
+bodys =[]
 
-for i in range(0, len(t), 1):
+for i in range(0,len(t),1):
+    body1, = plt.plot(sol[:i,0],sol[:i,2],'-',color='r')
+    body1_line, = plt.plot(sol[i,0],sol[i,2],'o',color='r')
     
-    sun, = plt.plot([0], [0], 'yo', ms=10)
+    body2, = plt.plot(sol[:i,4],sol[:i,6],'-',color='g')
+    body2_line, = plt.plot(sol[i,4],sol[i,6],'o',color='g')
     
-    neptun, = plt.plot(sol[:i, 0], sol[:i,2], 'r-')
-    neptun_line, = plt.plot(sol[i,0], sol[i,2], 'ro')
+    body3, = plt.plot(sol[:i,8],sol[:i,10],'-',color='b')
+    body3_line, = plt.plot(sol[i,8],sol[i,10],'o',color='b')
     
-    plyton, = plt.plot(sol[:i,4], sol[:i,6], 'b-')
-    plyton_line, = plt.plot(sol[i,4], sol[i,6], 'bo')
+    bodys.append([body1, body1_line,body2, body2_line,body3, body3_line])
     
-    planets.append([sun,neptun,neptun_line,plyton,plyton_line])
-    
-ani = ArtistAnimation(fig,planets,interval = 50)
+ani = ArtistAnimation(fig,bodys,interval=50)
 plt.axis('equal')
-ani.save('neptun_plyton.gif')
+ani.save('GG.gif')    
 
 
 
-
-
-
-
-
-#
+                                              
